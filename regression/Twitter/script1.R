87497c3c21b7797440b94c44e478d92879859553
Utkagr <agrawalutkarsh5@gmail.com>
2016-10-26 12:31:31 +0530

    data

---
+++ regression/Twitter/script1.R
@@ -0,0 +1,3 @@
+data_t <- read.table('/home/utkarsh/rtsm/regression/Twitter/Twitter.data',fileEncoding = "UTF-16",dec = ",")
+tdata <- read.csv("/home/utkarsh/rtsm/regression/Twitter/Twitter.data")
+View(tdata)
